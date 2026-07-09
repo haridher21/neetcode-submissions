69b9367a3790b6e77bbfd8e275dfe7b88a666761
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2026-07-09 11:25:28 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l = 0
+        d = {'(': 1, '{': 2, '[': 3, ')': 6, '}': 7, ']': 8}
+        for i in s:
+            print(l)
+            if d[i] < 4:
+                l  = l * 10 + d[i]
+            else:
+                if l == 0:
+                    return False
+                if l % 10 != d[i] - 5:
+                    return False
+                else:
+                    l = l // 10
+        if l:
+            return False
+        return True
